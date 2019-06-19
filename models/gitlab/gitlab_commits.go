ad30553d6fb9d23b42580345544c6b8c2f2fbe1b
Humaid AlQassimi <me@humaidq.ae>
2019-06-19 11:50:10 +0400

    Gitlab models will be replaced with gitlab.com/xanzy/go-gitlab No need to reinvent the wheel

--- models/gitlab/gitlab_commits.go
+++
@@ -1,7 +0,0 @@
-package gitlab
-
-type GitlabRepoCommits struct {
-	ID         string `json:"id"`
-	Title      string `json:"title"`
-	AuthorName string `json:"author_name"`
-}
--- models/gitlab/gitlab_project.go
+++
@@ -1,9 +0,0 @@
-package gitlab
-
-type GitLabProject struct {
-	ID                int    `json:"id"`
-	Name              string `json:"name"`
-	Description       string `json:"description"`
-	NameWithNamespace string `json:"name_with_namespace"`
-	WebURL            string `json:"web_url"`
-}
